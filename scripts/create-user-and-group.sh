cc5a97716d4653fc71afa5b78c867516d1384f60
Tobias Schneck <tobias.schneck@loodse.com>
2024-11-12 00:12:10 +0100

    add first darft of updated vsphere credentials, but not tested

--- scripts/create-user-and-group.sh
+++ scripts/create-user-and-group.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-govc sso.group.create -d="Group for Seed-Cluster Users" joshy-test-seed
-govc sso.group.create -d="Group for User-Cluster Users" joshy-test-user-cluster
+govc sso.group.create -d="Group for Seed-Cluster Users" example-ccm-user
+govc sso.group.create -d="Group for User-Cluster Users" example-infra-management-user
 
-govc sso.user.create -p "randompw" -R RegularUser -d "joshy-test-seed-user" joshy-test-seed-user
-govc sso.group.update -a joshy-test-seed-user joshy-test-seed
+govc sso.user.create -p "randompw" -R RegularUser -d "example-ccm-user" example-ccm-user
+govc sso.group.update -a example-ccm-user example-ccm-user
 
-govc sso.user.create -p "randompw" -R RegularUser -d "joshy-test-user-cluster-user" joshy-test-user-cluster-user
-govc sso.group.update -a joshy-test-user-cluster-user joshy-test-user-cluster
+govc sso.user.create -p "randompw" -R RegularUser -d "example-infra-management-user" example-infra-management-user
+govc sso.group.update -a example-infra-management-user example-infra-management-user
